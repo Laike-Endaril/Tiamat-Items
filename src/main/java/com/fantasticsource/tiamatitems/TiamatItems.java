0319c16add4ebacc5e9dd0302e9c5da05d0925dd
Kyle Koder <silvertallon@gmail.com>
2019-11-27 22:56:57 -0800

    Initial commit

---
+++ src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
@@ -0,0 +1,29 @@
+package com.fantasticsource.tiamatitems;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = TiamatItems.MODID, name = TiamatItems.NAME, version = TiamatItems.VERSION)
+public class TiamatItems
+{
+    public static final String MODID = "tiamatitems";
+    public static final String NAME = "Tiamat Items";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(TiamatItems.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}
