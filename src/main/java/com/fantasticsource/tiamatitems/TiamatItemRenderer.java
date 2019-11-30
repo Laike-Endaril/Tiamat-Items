16019709a6c7afb7cceb44b671bcaabde8678412
Kyle Koder <silvertallon@gmail.com>
2019-11-29 20:55:49 -0800

    Found the right transforms, and removed extra code for enchantment glint for now

--- src/main/java/com/fantasticsource/tiamatitems/TiamatItemRenderer.java
+++ src/main/java/com/fantasticsource/tiamatitems/TiamatItemRenderer.java
@@ -3,33 +3,23 @@ package com.fantasticsource.tiamatitems;
 import com.evilnotch.iitemrender.handlers.IItemRenderer;
 import com.evilnotch.iitemrender.handlers.IItemRendererHandler;
 import com.fantasticsource.tools.datastructures.Pair;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.ForgeHooksClient;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 
-import static com.evilnotch.iitemrender.handlers.IItemRendererHandler.allowEnchants;
-import static com.evilnotch.iitemrender.handlers.IItemRendererHandler.applyTransforms;
 import static com.fantasticsource.tiamatitems.TiamatItems.MODID;
 import static org.lwjgl.opengl.GL11.GL_QUADS;
 
 public class TiamatItemRenderer implements IItemRenderer
 {
-    private static final ResourceLocation RES_ITEM_GLINT = new ResourceLocation("textures/misc/enchanted_item_glint.png");
     public static final VertexFormat VOXEL = new VertexFormat();
     private static LinkedHashMap<String, LinkedHashMap<Integer, String>> readyTextures = new LinkedHashMap<>();
 
@@ -42,38 +32,10 @@ public class TiamatItemRenderer implements IItemRenderer
     public void render(ItemStack stack, IBakedModel model, ItemCameraTransforms.TransformType transformType, float v)
     {
         NBTTagCompound mainTag = stack.getTagCompound();
+
         if (mainTag == null || !mainTag.hasKey(MODID))
         {
-            //START TEST CODE
-
-
-            int color = 0x99ff0000;
-
-            boolean cachedEnch = allowEnchants;
-            allowEnchants = true;
-            GlStateManager.enableCull();
-
-            GlStateManager.pushMatrix();
-            if (IItemRendererHandler.isRunning)
-            {
-                GlStateManager.translate(0.5, 0.5, 0.5);
-                applyTransforms(model);
-                renderItem(stack, model, color);
-            }
-            else
-            {
-                applyTransforms(model);
-                renderItem(stack, model, color);
-            }
-            GlStateManager.popMatrix();
-
-            GlStateManager.disableCull();
-            allowEnchants = cachedEnch;
-
-
-            //END TEST CODE
-
-
+            IItemRendererHandler.renderItemStack(stack, model);
             return;
         }
 
@@ -96,8 +58,24 @@ public class TiamatItemRenderer implements IItemRenderer
 
         for (String key : new String[]{"r", "g", "b"})
         {
-            //START TEST CODE
+//            if (!compound.hasKey(key)) continue;
+//
+//            String recoloredTexture = recoloredTextures.computeIfAbsent(compound.getInteger(key), o ->
+//            {
+//                StringBuilder name = new StringBuilder(textureName);
+//                int color = compound.getInteger(key);
+//                String colorString = Integer.toHexString(color);
+//
+//                for (int i = colorString.length(); i < 6; i++) name.append("0");
+//                name.append(colorString);
+//
+//                return name.toString();
+//            });
 
+
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(0.5, 0.5, 0.5);
+            IItemRendererHandler.applyTransforms(model);
 
             GlStateManager.enableCull();
             GlStateManager.disableLighting();
@@ -113,23 +91,7 @@ public class TiamatItemRenderer implements IItemRenderer
             GlStateManager.enableLighting();
             GlStateManager.disableCull();
 
-
-            //END TEST CODE
-
-
-//            if (!compound.hasKey(key)) continue;
-//
-//            String recoloredTexture = recoloredTextures.computeIfAbsent(compound.getInteger(key), o ->
-//            {
-//                StringBuilder name = new StringBuilder(textureName);
-//                int color = compound.getInteger(key);
-//                String colorString = Integer.toHexString(color);
-//
-//                for (int i = colorString.length(); i < 6; i++) name.append("0");
-//                name.append(colorString);
-//
-//                return name.toString();
-//            });
+            GlStateManager.popMatrix();
         }
     }
 
@@ -142,88 +104,9 @@ public class TiamatItemRenderer implements IItemRenderer
     private static void voxel(BufferBuilder bufferBuilder, double x, double y, double z, int r, int g, int b, int a)
     {
         //THE BUFFERBUILDER ONLY USES COLOR PER-QUAD, NOT PER-VERTEX!  IT WILL ALWAYS USE THE COLOR OF THE LAST VERTEX IN THE QUAD FOR THE ENTIRE QUAD!
-        bufferBuilder.pos(0, 0, 0).color(r, g, b, a).endVertex();
-        bufferBuilder.pos(1, 0, 0).color(r, g, b, a).endVertex();
-        bufferBuilder.pos(1, 1, 0).color(r, g, b, a).endVertex();
-        bufferBuilder.pos(0, 1, 0).color(r, g, b, a).endVertex();
-    }
-
-
-    private void renderItem(ItemStack stack, IBakedModel model, int color)
-    {
-        if (!stack.isEmpty())
-        {
-            GlStateManager.pushMatrix();
-            GlStateManager.translate(-0.5, -0.5, -0.5);
-
-            if (model.isBuiltInRenderer())
-            {
-                GlStateManager.color(1, 1, 1, 1);
-                GlStateManager.enableRescaleNormal();
-                stack.getItem().getTileEntityItemStackRenderer().renderByItem(stack);
-            }
-            else
-            {
-                renderModel(model, color);
-
-                if (stack.hasEffect())
-                {
-                    renderEffect(model);
-                }
-            }
-
-            GlStateManager.popMatrix();
-        }
-    }
-
-    private void renderEffect(IBakedModel model)
-    {
-        TextureManager textureManager = Minecraft.getMinecraft().getTextureManager();
-
-        GlStateManager.depthMask(false);
-        GlStateManager.depthFunc(514);
-        GlStateManager.disableLighting();
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_COLOR, GlStateManager.DestFactor.ONE);
-        textureManager.bindTexture(RES_ITEM_GLINT);
-        GlStateManager.matrixMode(5890);
-        GlStateManager.pushMatrix();
-        GlStateManager.scale(8, 8, 8);
-        float f = (float) (Minecraft.getSystemTime() % 3000) / 3000 / 8;
-        GlStateManager.translate(f, 0, 0);
-        GlStateManager.rotate(-50, 0, 0, 1);
-        renderModel(model, -8372020);
-        GlStateManager.popMatrix();
-        GlStateManager.pushMatrix();
-        GlStateManager.scale(8, 8, 8);
-        float f1 = (float) (Minecraft.getSystemTime() % 4873) / 4873 / 8;
-        GlStateManager.translate(-f1, 0, 0);
-        GlStateManager.rotate(10, 0, 0, 1);
-        renderModel(model, -8372020);
-        GlStateManager.popMatrix();
-        GlStateManager.matrixMode(5888);
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-        GlStateManager.enableLighting();
-        GlStateManager.depthFunc(515);
-        GlStateManager.depthMask(true);
-        textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-    }
-
-    private void renderModel(IBakedModel model, int color)
-    {
-        List<BakedQuad> quads = model.getQuads(null, null, 0);
-        if (quads.size() == 0) return;
-
-
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferbuilder = tessellator.getBuffer();
-        bufferbuilder.begin(GL_QUADS, quads.get(0).getFormat());
-
-        for (BakedQuad quad : quads)
-        {
-            bufferbuilder.addVertexData(quad.getVertexData());
-            ForgeHooksClient.putQuadColor(bufferbuilder, quad, color);
-        }
-
-        tessellator.draw();
+        bufferBuilder.pos(-0.5, -0.5, 0).color(r, g, b, a).endVertex();
+        bufferBuilder.pos(0.5, -0.5, 0).color(r, g, b, a).endVertex();
+        bufferBuilder.pos(0.5, 0.5, 0).color(r, g, b, a).endVertex();
+        bufferBuilder.pos(-0.5, 0.5, 0).color(r, g, b, a).endVertex();
     }
 }
