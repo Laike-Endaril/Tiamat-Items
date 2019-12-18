92325707d03dae1519b151e208ae8e81835b6273
Kyle Koder <silvertallon@gmail.com>
2019-12-18 13:55:13 -0800

    Updated TODOs in TiamatItem class

--- src/main/java/com/fantasticsource/tiamatitems/TiamatItem.java
+++ src/main/java/com/fantasticsource/tiamatitems/TiamatItem.java
@@ -1,9 +1,14 @@
 package com.fantasticsource.tiamatitems;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 import static com.fantasticsource.tiamatitems.TiamatItems.MODID;
 
@@ -20,8 +25,16 @@ public class TiamatItem extends Item
     @Override
     public boolean isValidArmor(ItemStack stack, EntityEquipmentSlot armorType, Entity entity)
     {
-        //Vanilla armor slot support
+        //Vanilla equipment slot support
+        //Tiamat equipment slot support
+        //Baubles equipment slot support?
         //TODO
         return true;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        //TODO
     }
 }
