4e8e813271d89cdd79c4d23982a2449cb9cadaf2
Kyle Koder <silvertallon@gmail.com>
2019-12-18 14:43:44 -0800

    GUIItemLayer

--- src/main/java/com/fantasticsource/tiamatitems/itemeditor/GUIItemLayer.java
+++ src/main/java/com/fantasticsource/tiamatitems/itemeditor/GUIItemLayer.java
@@ -1,5 +1,18 @@
 package com.fantasticsource.tiamatitems.itemeditor;
 
-public class GUIItemLayer
+import com.fantasticsource.mctools.gui.GUIScreen;
+import com.fantasticsource.mctools.gui.element.textured.GUIItemStack;
+import com.fantasticsource.tiamatitems.TiamatItem;
+
+public class GUIItemLayer extends GUIItemStack
 {
+    public GUIItemLayer(GUIScreen screen, double unscaledWidth, double unscaledHeight, String layer)
+    {
+        super(screen, unscaledWidth, unscaledHeight, TiamatItem.get(false, false, layer));
+    }
+
+    public GUIItemLayer(GUIScreen screen, double x, double y, double unscaledWidth, double unscaledHeight, String layer)
+    {
+        super(screen, x, y, unscaledWidth, unscaledHeight, TiamatItem.get(false, false, layer));
+    }
 }
