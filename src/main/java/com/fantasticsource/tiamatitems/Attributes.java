bc81aa4cbf32f167fb3da3fed2b8b26cd0deed8e
Kyle Koder <silvertallon@gmail.com>
2020-01-04 02:09:29 -0800

    Some tiamat slots working, and doesn't crash when tiamat is not installed.  Still need to handle 2H slotting and check some others

--- src/main/java/com/fantasticsource/tiamatitems/Attributes.java
+++ src/main/java/com/fantasticsource/tiamatitems/Attributes.java
@@ -1,14 +1,17 @@
 package com.fantasticsource.tiamatitems;
 
+import com.fantasticsource.fantasticlib.Compat;
 import com.fantasticsource.mctools.Slottings;
 import com.fantasticsource.tiamatitems.nbt.AttributeTags;
 import com.fantasticsource.tiamatitems.nbt.SlottingTags;
+import com.fantasticsource.tiamatrpg.inventory.TiamatPlayerInventory;
 import com.fantasticsource.tools.Tools;
 import com.fantasticsource.tools.datastructures.Pair;
 import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -51,6 +54,8 @@ public class Attributes
         for (EntityPlayerMP player : server.getPlayerList().getPlayers())
         {
             AbstractAttributeMap attributeMap = player.getAttributeMap();
+
+            //Vanilla slots
             for (int slot = 0; slot < player.inventory.getSizeInventory(); slot++)
             {
                 for (IAttributeInstance attributeInstance : attributeMap.getAllAttributes().toArray(new IAttributeInstance[0]))
@@ -82,6 +87,52 @@ public class Attributes
 
                         IAttributeInstance attributeInstance = attributeMap.getAttributeInstanceByName(pair.getKey());
                         if (attributeInstance != null) attributeInstance.applyModifier(pair.getValue());
+                    }
+                }
+            }
+
+            //TODO Baubles slots
+            if (Compat.baubles)
+            {
+
+            }
+
+            //Tiamat slots
+            if (Compat.tiamatrpg)
+            {
+                IInventory inventory = TiamatPlayerInventory.tiamatServerInventories.get(player.getPersistentID());
+                for (int slot = 0; slot < inventory.getSizeInventory(); slot++)
+                {
+                    for (IAttributeInstance attributeInstance : attributeMap.getAllAttributes().toArray(new IAttributeInstance[0]))
+                    {
+                        attributeInstance.removeModifier(getTiamatModIDForSlot(slot + Slottings.TIAMAT_OFFSET));
+                    }
+
+                    ItemStack stack = inventory.getStackInSlot(slot);
+                    boolean valid = false;
+                    for (String slotting : SlottingTags.getItemSlottings(stack))
+                    {
+                        if (Slottings.slotValidForSlotting(slotting, slot + Slottings.TIAMAT_OFFSET, player))
+                        {
+                            valid = true;
+                            break;
+                        }
+                    }
+                    if (valid)
+                    {
+                        for (String modString : AttributeTags.getItemAttributeMods(stack))
+                        {
+                            Pair<String, AttributeModifier> pair = getTiamatModifierForSlot(slot + Slottings.TIAMAT_OFFSET, modString);
+                            if (pair == null)
+                            {
+                                System.err.println("WARNING: Quite possibly corrupted item; bad attribute modifier string: " + modString);
+                                System.err.println("Player: " + player.getName() + ", Itemname: " + stack.getDisplayName() + ", Tiamat Slot: " + slot);
+                                continue;
+                            }
+
+                            IAttributeInstance attributeInstance = attributeMap.getAttributeInstanceByName(pair.getKey());
+                            if (attributeInstance != null) attributeInstance.applyModifier(pair.getValue());
+                        }
                     }
                 }
             }
