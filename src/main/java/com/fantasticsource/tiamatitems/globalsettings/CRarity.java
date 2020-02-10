4ad76e26b1fc596f232dcd7f71df134e37282a61
Kyle Koder <silvertallon@gmail.com>
2020-02-10 07:26:12 -0800

    Added blacklist to CRarity

--- src/main/java/com/fantasticsource/tiamatitems/globalsettings/CRarity.java
+++ src/main/java/com/fantasticsource/tiamatitems/globalsettings/CRarity.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.common.network.ByteBufUtils;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class CRarity extends Component
 {
@@ -17,6 +18,7 @@ public class CRarity extends Component
     public Color color = Color.WHITE;
     public TextFormatting textColor = TextFormatting.WHITE;
     public CAttributePool attributePool = new CAttributePool();
+    public ArrayList<String> randomAttributeGenBlacklist = new ArrayList<>();
 
 
     @Override
@@ -26,6 +28,9 @@ public class CRarity extends Component
         buf.writeInt(color.color());
         buf.writeInt(textColor.getColorIndex());
         attributePool.write(buf);
+
+        buf.writeInt(randomAttributeGenBlacklist.size());
+        for (String s : randomAttributeGenBlacklist) ByteBufUtils.writeUTF8String(buf, s);
 
         return this;
     }
@@ -38,14 +43,18 @@ public class CRarity extends Component
         textColor = TextFormatting.fromColorIndex(buf.readInt());
         attributePool.read(buf);
 
+        randomAttributeGenBlacklist.clear();
+        for (int i = buf.readInt(); i > 0; i--) randomAttributeGenBlacklist.add(ByteBufUtils.readUTF8String(buf));
+
         return this;
     }
 
     @Override
     public CRarity save(OutputStream stream)
     {
-        new CStringUTF8().set(name).save(stream);
-        new CInt().set(color.color()).save(stream).set(textColor.getColorIndex()).save(stream);
+        CStringUTF8 cs = new CStringUTF8().set(name).save(stream);
+        new CInt().set(color.color()).save(stream).set(textColor.getColorIndex()).save(stream).set(randomAttributeGenBlacklist.size()).save(stream);
+        for (String s : randomAttributeGenBlacklist) cs.set(s).save(stream);
 
         return this;
     }
@@ -53,12 +62,16 @@ public class CRarity extends Component
     @Override
     public CRarity load(InputStream stream)
     {
-        name = new CStringUTF8().load(stream).value;
+        CStringUTF8 cs = new CStringUTF8();
+        name = cs.load(stream).value;
 
         CInt ci = new CInt();
         color.setColor(ci.load(stream).value);
         textColor = TextFormatting.fromColorIndex(ci.load(stream).value);
         attributePool.load(stream);
+
+        randomAttributeGenBlacklist.clear();
+        for (int i = ci.load(stream).value; i > 0; i--) randomAttributeGenBlacklist.add(cs.load(stream).value);
 
         return this;
     }
