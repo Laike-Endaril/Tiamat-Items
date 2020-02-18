e1bcc2c8a74bb826e20ac4eedfde7c4ab7829748
Kyle Koder <silvertallon@gmail.com>
2020-02-18 05:06:54 -0800

    TODO update

--- src/main/java/com/fantasticsource/tiamatitems/assembly/ItemAssembly.java
+++ src/main/java/com/fantasticsource/tiamatitems/assembly/ItemAssembly.java
@@ -1,5 +1,14 @@
 package com.fantasticsource.tiamatitems.assembly;
 
+import net.minecraft.item.ItemStack;
+
 public class ItemAssembly
 {
+    public static void putPartInSlot(ItemStack core, ItemStack part, int slot)
+    {
+        //TODO Check validity
+        //TODO If going from empty to not empty, store the empty version of the core internally
+        //TODO Apply part to slot in NBT
+        //TODO transform the core item
+    }
 }
