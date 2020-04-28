08da12fe39e501c56b78e4997b28d43c6e0c5374
Kyle Koder <silvertallon@gmail.com>
2020-04-28 09:51:29 -0700

    1.12.2.000l; removed remote version requirement temporarily

--- src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
+++ src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
@@ -49,12 +49,12 @@ import net.minecraftforge.registries.IForgeRegistry;
 import java.io.IOException;
 import java.util.List;
 
-@Mod(modid = TiamatItems.MODID, name = TiamatItems.NAME, version = TiamatItems.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034l,);required-after:tiamatactions@[1.12.2.000,)")
+@Mod(modid = TiamatItems.MODID, name = TiamatItems.NAME, version = TiamatItems.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034l,);required-after:tiamatactions@[1.12.2.000,)", acceptableRemoteVersions = "*")
 public class TiamatItems
 {
     public static final String MODID = "tiamatitems";
     public static final String NAME = "Tiamat Items";
-    public static final String VERSION = "1.12.2.000k";
+    public static final String VERSION = "1.12.2.000l";
     public static final String DOMAIN = "tiamatrpg";
 
 
