ac8073e79be8f6ffd590aa5f50551b410b09bab4
Kyle Koder <silvertallon@gmail.com>
2020-05-02 08:09:24 -0700

    Revert "1.12.2.000r"
    This reverts commit da38c65ffedb4793c859406ed9bbfdb986346e13.

--- src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
+++ src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
@@ -54,7 +54,7 @@ public class TiamatItems
 {
     public static final String MODID = "tiamatitems";
     public static final String NAME = "Tiamat Items";
-    public static final String VERSION = "1.12.2.000r";
+    public static final String VERSION = "1.12.2.000q";
 
 
     public static final FilterRangedInt FILTER_POSITIVE = FilterRangedInt.get(0, Integer.MAX_VALUE);
