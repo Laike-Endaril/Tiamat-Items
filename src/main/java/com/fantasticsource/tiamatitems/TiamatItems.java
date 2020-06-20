380bef62327dd72784933270b4dde42950ef4bee
Kyle Koder <silvertallon@gmail.com>
2020-06-19 18:13:35 -0700

    Upped lib requirement to same version as is in my dev environment, just in case

--- src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
+++ src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
@@ -50,7 +50,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 import java.io.IOException;
 import java.util.List;
 
-@Mod(modid = TiamatItems.MODID, name = TiamatItems.NAME, version = TiamatItems.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034l,);required-after:tiamatactions@[1.12.2.000,)")
+@Mod(modid = TiamatItems.MODID, name = TiamatItems.NAME, version = TiamatItems.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.034n,);required-after:tiamatactions@[1.12.2.000,)")
 public class TiamatItems
 {
     public static final String MODID = "tiamatitems";
