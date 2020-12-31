c52499d0e69d638cc8a51f47855c309e53cdb03c
Kyle Koder <silvertallon@gmail.com>
2020-12-31 13:18:46 -0800

    1.12.2.000zx

--- src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
+++ src/main/java/com/fantasticsource/tiamatitems/TiamatItems.java
@@ -49,12 +49,12 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 import java.io.IOException;
 
-@Mod(modid = TiamatItems.MODID, name = TiamatItems.NAME, version = TiamatItems.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.044i,);required-after:tiamatactions@[1.12.2.000zo,)")
+@Mod(modid = TiamatItems.MODID, name = TiamatItems.NAME, version = TiamatItems.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.044j,);required-after:tiamatactions@[1.12.2.000zo,)")
 public class TiamatItems
 {
     public static final String MODID = "tiamatitems";
     public static final String NAME = "Tiamat Items";
-    public static final String VERSION = "1.12.2.000zw";
+    public static final String VERSION = "1.12.2.000zx";
     public static final String DOMAIN = "tiamatrpg";
 
 
